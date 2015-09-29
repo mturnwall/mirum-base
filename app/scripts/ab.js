e773295c0d1ebb469e836efeab30b5b10ad3da55
Michael Turnwall <freelance@turnwall.net>
2015-09-28 17:07:36 -0700

    Add and configure all best practices eslint rules

--- app/scripts/ab.js
+++ app/scripts/ab.js
@@ -1,10 +1,24 @@
 let message = 'Hello World';
+let foo = message;
 const SET = 1111;
 
+// TODO
 (() => {
     return {
-        say: alert(message)
+        say: alert(message + foo)
     };
 })();
 
-
+// FIXME something is not right here
+function function_name (x)  {
+    x.y = 123;
+    if (typeof x === 'undefined') {
+        return x;
+    } else {
+        return false;
+    }
+}
+$('test')
+    .on('click')
+    .on('keyup');
+function_name();
