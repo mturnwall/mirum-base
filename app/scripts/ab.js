958a478fc8bcc428dcc72021025f2e2a6655e99e
Michael Turnwall <freelance@turnwall.net>
2015-09-29 17:00:25 -0700

    Finish style rules and update version of eslint

--- app/scripts/ab.js
+++ app/scripts/ab.js
@@ -1,5 +1,10 @@
-let message = 'Hello World';
-let foo = message;
+let message = 'Hello World',
+    foo = message,
+    arr = ['foo', 'bar'],
+    obj = {
+        foo: 'bar'
+    },
+    xy = foo !== message;
 const SET = 1111;
 
 console.log(foo);
@@ -7,13 +12,23 @@ console.log(foo);
 // TODO
 (() => {
     return {
-        say: message + foo,
+        say: message + foo
     };
 })();
 
-function function_name (x) {
-    var hello;
+if (arr) {
+    // do something
+} else {
+    if (!arr) {
+        // do something else
+    }
+}
+
+function function_name(ab) {
+    var hello,
+        that = this;
     x.y = 123;
+    that.y = 456;
     if (typeof x === 'undefined') {
         var world = x;
         return hello +  world;
@@ -21,6 +36,9 @@ function function_name (x) {
         return false;
     }
 }
+
+function blockSpacing() { return true; }
+
 $('test')
     .on('click')
     .on('keyup');
